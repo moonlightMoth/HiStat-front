5af251f361f307720c8d19dfdb626af0a9db4f2a
Sophia <lsofa1204@gmail.com>
2025-01-24 17:06:55 +0300

    merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-<<<<<<< HEAD
-=======
-
->>>>>>> f2e8a7ba06c757ba241fb9ebb91870741c90586d
   define: { _global: ({}), },
 
   base:'./'
